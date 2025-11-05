d117735783d61ea3f1f168471d4ab54516a6b07b
soufiane amabrra <manga.sfn@gmail.com>
2025-11-05 15:19:21 +0100

    feat: customize cmp and copilot keybindings
    - Tab accepts autocomplete selection, Enter free for typing
    - C-Space invokes completion, Esc aborts
    - C-Right accepts Copilot suggestions word by word

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.mapping = {
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        ["<Esc>"] = cmp.mapping.abort(),
+      }
+    end,
+  },
+}
