8d69e630b0a13ba8498e9e94b9412affbee9c0d4
soufiane amabrra <manga.sfn@gmail.com>
2025-11-10 04:40:23 +0100

    fixing yank by adding win32yank

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,20 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- Force Neovim to use win32yank for clipboard operations
+vim.g.clipboard = {
+  name = "win32yank",
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf",
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf",
+  },
+  cache_enabled = 0,
+}
+
+-- Ensure the 'unnamedplus' option is set to use the system clipboard
+vim.opt.clipboard = "unnamedplus"
+
